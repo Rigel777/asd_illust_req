4e7b048a2163c9902f2a4a54e1c3200b17574c36
佐竹涼太 <picoreamosaiko@docomo.ne.jp>
2018-10-15 12:44:56 +0900

    wip 途中までのテストコミット

--- test/main_test.rb
+++ test/main_test.rb
@@ -1,16 +1,13 @@
 ::RBNACL_LIBSODIUM_GEM_LIB_PATH = "libsodium.dll"
 require 'minitest/autorun'
 require 'open3'
+require 'rbnacl'
 require '../src/Main.rb'
 
 class MainTest < Minitest::Test
-    def setup
-        Open3.popen3("ruby" , "ping.rb")
-    end
     def test_sample
-        $bot.message(:'req!help') do |event|
-            assert_equal event.respond , a
-        end
+        $bot.send_message('aaa', '')
+        assert_equal message.respond , a
     end
 
     def test_sample2
